37000c7c4c46c34abe7e778a7f7ac605c4bff299
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-09-11 18:38:09 +0530

    deleted a value using its key

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -1,5 +1,5 @@
-data={1:'albert',2:'singh',5:'peter',4:'son'}#key value pair and should be immutable
-print(data[4])
+data={'a':'albert','b':'singh','c':'peter','d':'son'}#key value pair and should be immutable
+print(data['a'])
 #get() to fetch the values
 #pop() to remove the values
 print("using get() : ",data.get(2))
@@ -9,3 +9,9 @@ value=['flask','node','spring']
 #2 lists need to merge as a dictionary
 datas=dict(zip(key,value))#zip function adds the 2 lists and dict is to make the 'datas' as dictionary
 print(datas)
+
+# delete a value in the dictionary
+del datas['python']
+print(datas)
+del data['a']
+print(data)
