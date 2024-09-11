05ec9dadb0650aa9bd7933da1746c9013f67db8c
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-09-11 18:22:04 +0530

    retrived values using get() function

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -1,2 +1,5 @@
 data={1:'albert',2:'singh',5:'peter',4:'son'}#key value pair and should be immutable
 print(data[4])
+#get() to fetch the values
+#pop() to remove the values
+print("using get() : ",data.get(2))
