41e78b30c0a892287402111bdf797f13626e93b7
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-09-11 18:29:38 +0530

    using zip function added two values as key value pairs

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -3,3 +3,9 @@ print(data[4])
 #get() to fetch the values
 #pop() to remove the values
 print("using get() : ",data.get(2))
+
+key=['python','javascrtipt','java']
+value=['flask','node','spring']
+#2 lists need to merge as a dictionary
+datas=dict(zip(key,value))#zip function adds the 2 lists and dict is to make the 'datas' as dictionary
+print(datas)
