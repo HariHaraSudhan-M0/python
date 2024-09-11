04396b4daed68259f95d56e4f91344f14635bd9a
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-09-11 09:48:54 +0530

    added a element using insert function

--- lists/list.py
+++ lists/list.py
@@ -5,5 +5,8 @@ values=[19.5,'albert',876]#can store any datatype in lists
 print(values)
 
 
-num.append(96)
+num.append(96)#to add a element into the list
 print (num)
+
+num.insert(0,12)#add element at the 0th position and value added will be 12
+print(num)
