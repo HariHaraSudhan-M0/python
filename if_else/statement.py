443cb17685c51aeb1b0ec669eb139cd7ce2b8dd5
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-09-18 18:32:12 +0530

    nested conditional statement

--- if_else/statement.py
+++ if_else/statement.py
@@ -1,5 +1,13 @@
 age=int(input("enter your age:"))
 if (age>=18 and age<=60):
+    if(age>50):
+        print("elder person")
     print("eligible")
+
+elif(age>60):
+    print("senior citizen")
+    if(age>100):
+        print("dont lie")
+
 else:
-    print("not eligible")
+    print("too young")
